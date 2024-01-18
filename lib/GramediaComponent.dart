922c9a55527c72a7d451217be6dc0e7c4cd1127c
Chondro Satrio Wibowo <chondro.satrio@gramedia.id>
2024-01-18 12:33:52 +0700

    add exported library

---
+++ lib/GramediaComponent.dart
@@ -0,0 +1,12 @@
+
+/// Base Core
+export 'package:papilus_component_gramedia/Core/Color/Color.dart';
+export 'package:papilus_component_gramedia/Core/Radius/Radius.dart';
+export 'package:papilus_component_gramedia/Core/Typography/Typography.dart';
+export 'package:papilus_component_gramedia/Core/Spacing/Spacing.dart';
+
+
+/// Component
+export 'package:papilus_component_gramedia/Bottomsheet/Bottomsheets.dart';
+export 'package:papilus_component_gramedia/Text/GramediaText.dart';
+export 'package:papilus_component_gramedia/SegmentedControlled/SegmentedControlled.dart';
